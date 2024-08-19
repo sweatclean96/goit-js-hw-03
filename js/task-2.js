b32aa2308c8865a78c24d2e48ada85349d11359d
sweatclean96 <9336208+sweatclean96@users.noreply.github.com>
2024-08-19 22:03:25 +0300

    Update task-2.js
    curly brackets

--- js/task-2.js
+++ js/task-2.js
@@ -6,6 +6,7 @@ function makeArray(firstArray, secondArray, maxLength) {
   } else {
     return thirdArray;
   }
+}
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
